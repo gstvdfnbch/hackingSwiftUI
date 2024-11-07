58e7298630c3cd47f9c54c65ac3d20efdf6f8698
gstv <gustavodiefenbach@gmail.com>
2024-11-07 08:14:25 -0300

    first commit

---
+++ hackingSwiftUI/hackingSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  hackingSwiftUIApp.swift
+//  hackingSwiftUI
+//
+//  Created by Gustavo Diefenbach on 07/11/24.
+//
+
+import SwiftUI
+
+@main
+struct hackingSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
