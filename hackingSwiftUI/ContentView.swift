8cc58eb4c0c266ed56e44e3946cd0d4afd932ee9
gstv <gustavodiefenbach@gmail.com>
2024-11-07 10:14:27 -0300

    Add scroll to the view

--- hackingSwiftUI/ContentView.swift
+++ hackingSwiftUI/ContentView.swift
@@ -8,78 +8,80 @@ struct ContentView: View {
     
     var body: some View {
         ScrollView {
-            VStack(spacing: 32) {
-                Text("Convert Celsius to Farehenheit")
-                    .font(.largeTitle)
-                    .fontWeight(.bold)
-                    .lineLimit(2)
-                
-                
-                HStack(spacing: 16) {
-                    DisplayValue(value: temperatureCelsius,
-                                 unit: .celsius,
-                                 color: .orange)
-                    
-                    DisplayValue(value: resultTemperatura,
-                                 unit: .farhenheit,
-                                 color: .green)
-                }
-                            
-                VStack(spacing: 8.0) {
-                    
-                    HStack {
-                        Text("Insert temperature:")
-                        Spacer()
-                    }
-                    
-                    HStack{
-                        TextField("Temperature in Celsius", text: $inputTemperature)
-                            .foregroundStyle(.gray)
-                            .font(.title)
-                            .keyboardType(.decimalPad)
-                            .onChange(of: inputTemperature) { newValue in
-                                if let result = Double(newValue) {
-                                    temperatureCelsius = result
-                                } else {
-                                    temperatureCelsius = 0.0
-                                }
-                            }
-                    
-                        Text("°C")
-                            .foregroundStyle(self.inputTemperature.isEmpty ? .black.opacity(0.2) : .gray)
-                            .font(.title3)
-                    }
-                    .padding(16)
-                    .background {
-                        RoundedRectangle(cornerRadius: 12)
-                            .fill(.black.opacity(0.1))
-                    }
-                }
-                
-                Button(action: {
-                    self.calculete()
-                }) {
-                    HStack {
-                        Spacer()
-                        
-                        Text("Calculate")
-                            .disabled(inputTemperature.isEmpty)
-                            .foregroundStyle(.white)
-                            .padding(16)
-                            .font(.title2)
-                            .fontWeight(.bold)
-                        
-                        Spacer()
-                    }
-                    .background(self.inputTemperature.isEmpty ? .gray : .blue)
-                    .cornerRadius(12)
-                }
-                
-            }
-            .multilineTextAlignment(.center)
-            .padding(16)
-        }
+                VStack(spacing: 32) {
+                    Spacer()
 
+                    Text("Convert Celsius to Farehenheit")
+                        .font(.largeTitle)
+                        .fontWeight(.bold)
+                        .lineLimit(2)
+                    
+                    
+                    HStack(spacing: 16) {
+                        DisplayValue(value: temperatureCelsius,
+                                     unit: .celsius,
+                                     color: .orange)
+                        
+                        DisplayValue(value: resultTemperatura,
+                                     unit: .farhenheit,
+                                     color: .green)
+                    }
+                                
+                    VStack(spacing: 8.0) {
+                        
+                        HStack {
+                            Text("Insert temperature:")
+                            Spacer()
+                        }
+                        
+                        HStack{
+                            TextField("Temperature in Celsius", text: $inputTemperature)
+                                .foregroundStyle(.gray)
+                                .font(.title)
+                                .keyboardType(.decimalPad)
+                                .onChange(of: inputTemperature) { newValue in
+                                    if let result = Double(newValue) {
+                                        temperatureCelsius = result
+                                    } else {
+                                        temperatureCelsius = 0.0
+                                    }
+                                }
+                        
+                            Text("°C")
+                                .foregroundStyle(self.inputTemperature.isEmpty ? .black.opacity(0.2) : .gray)
+                                .font(.title3)
+                        }
+                        .padding(16)
+                        .background {
+                            RoundedRectangle(cornerRadius: 12)
+                                .fill(.black.opacity(0.1))
+                        }
+                    }
+
+                    Spacer()
+
+                    Button(action: {
+                        self.calculete()
+                    }) {
+                        HStack {
+                            Spacer()
+                            
+                            Text("Calculate")
+                                .disabled(inputTemperature.isEmpty)
+                                .foregroundStyle(.white)
+                                .padding(16)
+                                .font(.title2)
+                                .fontWeight(.bold)
+                            
+                            Spacer()
+                        }
+                        .background(self.inputTemperature.isEmpty ? .gray : .blue)
+                        .cornerRadius(12)
+                    }
+                }
+                .multilineTextAlignment(.center)
+                .padding(16)
+            }
     }
     
     func calculete(){
