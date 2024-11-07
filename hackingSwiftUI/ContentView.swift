b2f4d4b07fadb429b0056b403808bddb4e1b115d
gstv <gustavodiefenbach@gmail.com>
2024-11-07 09:42:10 -0300

    Make adjust on the layout

--- hackingSwiftUI/ContentView.swift
+++ hackingSwiftUI/ContentView.swift
@@ -3,79 +3,82 @@ import SwiftUI
 struct ContentView: View {
     @State var inputTemperature: String = ""
     
-    @State var temperatureCelsius: Double = 0.0 //Celsius
+    @State var temperatureCelsius: Double = 0.0
     @State var resultTemperatura: Double = 0.0
     
     var body: some View {
-        VStack(spacing: 32) {
-            Spacer()
-
-            Text("Convert Celsius to Farehenheit")
-                .font(.largeTitle)
-                .fontWeight(.bold)
-            
-            Spacer()
-            
-            HStack(spacing: 16) {
-                DisplayValue(value: temperatureCelsius, unit: .celsius, color: .orange)
-                DisplayValue(value: resultTemperatura, unit: .farhenheit, color: .green)
-            }
-                        
-            VStack(spacing: 8.0) {
+        ScrollView {
+            VStack(spacing: 32) {
+                Text("Convert Celsius to Farehenheit")
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                    .lineLimit(2)
                 
-                HStack {
-                    Text("Insert temperature:")
-                    Spacer()
+                
+                HStack(spacing: 16) {
+                    DisplayValue(value: temperatureCelsius,
+                                 unit: .celsius,
+                                 color: .orange)
+                    
+                    DisplayValue(value: resultTemperatura,
+                                 unit: .farhenheit,
+                                 color: .green)
                 }
-                
-                HStack{
-                    TextField("Temperature in Celsius", text: $inputTemperature)
-                        .foregroundStyle(.black)
-                        .font(.title)
-                        .keyboardType(.decimalPad)
-                        .onChange(of: inputTemperature) { newValue in
-                            if let result = Double(newValue) {
-                                temperatureCelsius = result
-                            } else {
-                                temperatureCelsius = 0.0
+                            
+                VStack(spacing: 8.0) {
+                    
+                    HStack {
+                        Text("Insert temperature:")
+                        Spacer()
+                    }
+                    
+                    HStack{
+                        TextField("Temperature in Celsius", text: $inputTemperature)
+                            .foregroundStyle(.gray)
+                            .font(.title)
+                            .keyboardType(.decimalPad)
+                            .onChange(of: inputTemperature) { newValue in
+                                if let result = Double(newValue) {
+                                    temperatureCelsius = result
+                                } else {
+                                    temperatureCelsius = 0.0
+                                }
                             }
-                        }
+                    
+                        Text("°C")
+                            .foregroundStyle(self.inputTemperature.isEmpty ? .black.opacity(0.2) : .gray)
+                            .font(.title3)
+                    }
+                    .padding(16)
+                    .background {
+                        RoundedRectangle(cornerRadius: 12)
+                            .fill(.black.opacity(0.1))
+                    }
+                }
                 
-                    Text("°C")
-                        .foregroundStyle(.black)
-                        .font(.title3)
+                Button(action: {
+                    self.calculete()
+                }) {
+                    HStack {
+                        Spacer()
+                        
+                        Text("Calculate")
+                            .disabled(inputTemperature.isEmpty)
+                            .foregroundStyle(.white)
+                            .padding(16)
+                            .font(.title2)
+                            .fontWeight(.bold)
+                        
+                        Spacer()
+                    }
+                    .background(self.inputTemperature.isEmpty ? .gray : .blue)
+                    .cornerRadius(12)
                 }
-                .padding(16)
-                .background {
-                    RoundedRectangle(cornerRadius: 12)
-                        .fill(.black.opacity(0.1))
-                }
+                
             }
-            
-            Button(action: {
-                self.calculete()
-            }) {
-                HStack {
-                    Spacer()
-                    
-                    Text("Calculate")
-                        .disabled(inputTemperature.isEmpty)
-                        .foregroundStyle(.white)
-                        .padding(16)
-                        .font(.title2)
-                        .fontWeight(.bold)
-                    
-                    Spacer()
-                }
-                .background(.blue)
-                .cornerRadius(12)
-            }
-            .disabled(self.inputTemperature.isEmpty)
+            .multilineTextAlignment(.center)
+            .padding(16)
         }
-        .multilineTextAlignment(.center)
-        .padding(16)
-
-        Spacer()
 
     }
     
