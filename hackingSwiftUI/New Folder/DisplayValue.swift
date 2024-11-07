58d9d227f1e25fa1ec3f5dc00f6fee5c1f25e6bb
gstv <gustavodiefenbach@gmail.com>
2024-11-07 09:05:44 -0300

    Create the component the show the values

--- hackingSwiftUI/New Folder/DisplayValue.swift
+++ hackingSwiftUI/New Folder/DisplayValue.swift
@@ -1,11 +1,48 @@
 import SwiftUI
 
 struct DisplayValue: View {
+    var text: String
+    var color: Color
+    var unit: UnitsTemperature
+    
+    init(value: Double, unit: UnitsTemperature, color: Color = .black) {
+        self.text = String(value)
+        self.color = color
+        self.unit = unit
+    }
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack {
+            Spacer()
+            
+            VStack(spacing: 4) {
+                HStack {
+                    Text(text)
+                        .foregroundStyle(.white)
+                        .font(.largeTitle)
+                        .fontWeight(.bold)
+                    
+                    Text(unit == .celsius ? "°C" : "°F")
+                        .foregroundStyle(.white)
+                        .font(.title)
+                        .fontWeight(.bold)
+                }
+                
+                Text(self.unit.rawValue)
+                    .foregroundStyle(.white)
+                    .font(.title3)
+            }
+            
+            Spacer()
+        }
+        .frame(height: 150)
+        .background(color)
+        .cornerRadius(20)
+        .shadow(radius: 10)
     }
 }
 
 #Preview {
-    DisplayValue(value: 20)
+    DisplayValue(value: 20, unit: .farhenheit, color: .yellow)
+        .padding(16)
 }
